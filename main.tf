1e329618352303aa45fbc024f1e5dce34092cab6
jrbeverly <jrbeverly@live.com>
2019-05-02 22:04:48 -0400

    Configures the S3 policy for CICD

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+resource "aws_iam_policy" "s3" {
+  count       = "${var.policy == "" ? 0 : 1}"
+  name        = "${var.name}"
+  description = "${var.description}"
+  path        = "${var.path}"
+  policy      = "${data.aws_iam_policy_document.s3_policy.json}"
+}
 
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid       = "ListBucket"
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["${var.bucket_arn}"]
+  }
+
+  statement {
+    sid       = "ReadWriteBucket"
+    effect    = "Allow"
+    actions   = ["s3:DeleteObject", "s3:GetObject", "s3:PutObject"]
+    resources = ["${var.bucket_arn}/${var.path}"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,28 @@
+variable "name" {
+  type        = "string"
+  description = "The name of the policy. If omitted, Terraform will assign a random, unique name."
+  default     = ""
+}
 
+variable "policy" {
+  type        = "string"
+  description = "Determines if the IAM Policy will be created."
+  default     = "true"
+}
+
+variable "description" {
+  type        = "string"
+  description = "Description of the IAM policy."
+  default     = "Allows deployment of build artifacts to S3."
+}
+
+variable "path" {
+  type        = "string"
+  description = "Path in which to create the policy. See IAM Identifiers for more information."
+  default     = "/ci"
+}
+
+variable "bucket_arn" {
+  type        = "list"
+  description = "A list of S3 bucket ARNs that this statement applies to."
+}
