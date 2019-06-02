551ce2d41b4808ce41791646f131a34f768e4e98
jrbeverly <jrbeverly@live.com>
2019-06-02 13:09:37 -0400

    Upgrade to terraform v0.12

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,41 @@
 output "policy_document" {
   description = "The above arguments serialized as a standard JSON policy document."
-  value       = "${data.aws_iam_policy_document.s3.json}"
+  value       = data.aws_iam_policy_document.s3.json
 }
 
 output "id" {
   description = "The policy's ID."
-  value       = "${data.aws_iam_policy_document.s3.id}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.id) : ""}"
+  value       = data.aws_iam_policy_document.s3.id
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.id) : ""
 }
 
 output "arn" {
   description = "The ARN assigned by AWS to this policy."
-  value       = "${data.aws_iam_policy_document.s3.arn}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.arn) : ""}"
+  value       = data.aws_iam_policy_document.s3.arn
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.arn) : ""
 }
 
 output "description" {
   description = "The description of the policy."
-  value       = "${data.aws_iam_policy_document.s3.description}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.description) : ""}"
+  value       = data.aws_iam_policy_document.s3.description
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.description) : ""
 }
 
 output "name" {
   description = "The name of the policy."
-  value       = "${data.aws_iam_policy_document.s3.name}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.name) : ""}"
+  value       = data.aws_iam_policy_document.s3.name
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.name) : ""
 }
 
 output "path" {
   description = "The path of the policy in IAM."
-  value       = "${data.aws_iam_policy_document.s3.path}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.path) : ""}"
+  value       = data.aws_iam_policy_document.s3.path
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.path) : ""
 }
 
 output "policy" {
   description = "The policy document."
-  value       = "${data.aws_iam_policy_document.s3.policy}"
-  value       = "${var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.policy) : ""}"
+  value       = data.aws_iam_policy_document.s3.policy
+  value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.policy) : ""
 }
+
--- variables.tf
+++ variables.tf
@@ -1,28 +1,29 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "The name of the policy. If omitted, Terraform will assign a random, unique name."
   default     = ""
 }
 
 variable "policy" {
-  type        = "string"
+  type        = string
   description = "Determines if the IAM Policy will be created."
   default     = true
 }
 
 variable "description" {
-  type        = "string"
+  type        = string
   description = "Description of the IAM policy."
   default     = "Allows deployment of build artifacts to S3."
 }
 
 variable "path" {
-  type        = "string"
+  type        = string
   description = "Path in which to create the policy. See IAM Identifiers for more information."
   default     = "/ci"
 }
 
 variable "bucket_arn" {
-  type        = "list"
+  type        = list(string)
   description = "A list of S3 bucket ARNs that this statement applies to."
 }
+
