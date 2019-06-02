51f274e92db6dc78da560d4a614d9e4b6044ce25
jrbeverly <jrbeverly@live.com>
2019-06-02 13:55:04 -0400

    Remove extra lines

--- outputs.tf
+++ outputs.tf
@@ -5,37 +5,31 @@ output "policy_document" {
 
 output "id" {
   description = "The policy's ID."
-  value       = data.aws_iam_policy_document.s3.id
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.id) : ""
 }
 
 output "arn" {
   description = "The ARN assigned by AWS to this policy."
-  value       = data.aws_iam_policy_document.s3.arn
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.arn) : ""
 }
 
 output "description" {
   description = "The description of the policy."
-  value       = data.aws_iam_policy_document.s3.description
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.description) : ""
 }
 
 output "name" {
   description = "The name of the policy."
-  value       = data.aws_iam_policy_document.s3.name
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.name) : ""
 }
 
 output "path" {
   description = "The path of the policy in IAM."
-  value       = data.aws_iam_policy_document.s3.path
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.path) : ""
 }
 
 output "policy" {
   description = "The policy document."
-  value       = data.aws_iam_policy_document.s3.policy
   value       = var.policy == "true" ? join("", data.aws_iam_policy_document.s3.*.policy) : ""
 }
 
